9ce77a3504853d93efd604b20696edda5d0da3de
Lucas <lsarazin@cogep.fr>
2023-01-16 11:44:25 +0100

    change view

---
+++ lang/fr/other.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'home' => 'Accueil',
+    'password' => 'Mauvais mot de passe',
+    'throttle' => 'Trop de tentatives échoués. Veuillez réessayer dans :seconds secondes.',
+];
