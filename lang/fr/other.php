bd353f5e3a8da03cb87fc9ab5773866f29900b4c
Edwin Roye <edwin.roye.auditeur@lecnam.net>
2023-01-16 16:12:22 +0100

    Translation part 3

--- lang/fr/other.php
+++ lang/fr/other.php
@@ -1,7 +1,5 @@
 <?php
 
 return [
-    'home' => 'Accueil',
-    'password' => 'Mauvais mot de passe',
-    'throttle' => 'Trop de tentatives échoués. Veuillez réessayer dans :seconds secondes.',
+    'home' => 'Tableau de bord',
 ];
