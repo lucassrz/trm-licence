f485d3528253a7ad337b365fd7d6dbb8ac70f35f
Nicolas BESNARD <nicolas.besnard.auditeur@lecnam.net>
2023-02-08 17:02:18 +0100

    Fonctionnement de la fonction recherche afin de rechercher un utilisateur par son nom ou son email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,23 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
-
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
-class UserController extends Controller
-{
-    public function users(Request $request): View
-    {
-        $users = User::sortable(
-            function ($query) use ($request) {
-                if (($s = $request->s)) {
-                    $query->orWhere('name', 'LIKE', '%' . $s . '%')
-                        ->orWhere('email', 'LIKE', '%' . $s . '%')
+class UserController extends Controller {
+
+     public function users(Request $request): View {
+
+         $criteria = $request->input('s');
+
+         $users = DB::table('users')
+                        ->where('name', 'LIKE', '%' . $criteria . '%')
+                        ->orWhere('email', 'LIKE', '%' . $criteria . '%')
                         ->get();
-                }
+
+        /* $query = User::query()->where('name', 'LIKE', '%' . $criteria . '%')
+                              ->where('email', 'LIKE', '%' . $criteria . '%')
+                              ->sortable()->paginate(10); */
+                              
+         /* $users = User::sortable( 
+
+            function ($query) use ($request) {
+
+                    $query->where('name', 'LIKE', '%' . $s . '%')
+                          ->orWhere('email', 'LIKE', '%' . $s . '%')
+                          ->get();
             }
-        )->paginate(10);
+
+        )->paginate(10); */
+
         
-        return view('users', ['users'=>$users]);
+        return view('users', ['users'=>$users->paginate(10)]);
     }
-}
+
+} 
